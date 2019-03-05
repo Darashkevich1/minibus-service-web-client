81ff4207add9332dc44fa272093ceac3e177ac84
Darashkevich1 <darashkevich.valentin@gmail.com>
2019-03-05 15:24:36 +0300

    Добавлена погода на главную страницу (для учебы)

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,16 @@
 var button_login = $("#button_login")
 var button_profile = $("#button_profile")
+var weather_info = $("#weather_info")
 
 if($.cookie("userId") != "null" && $.cookie("userId") != undefined) {
 	button_profile.show()
 } else {
 	button_login.show()
 }
+
+$.ajax({
+	url: "http://api.apixu.com/v1/current.json?key=319c202de86c434983c112021190503&q=Minsk",
+	success: function(data) {
+		weather_info.text("Город: " + data.location.name + ", температура: " + data.current.temp_c + " C, скорость ветра: " + data.current.wind_mph + " миль/ч")
+	}
+})
